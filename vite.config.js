4d011cb0d339646f5fe988e5d378ecf2611f296c
Joel0612 <https://github.com/Joel0612/Venta.git>
2025-09-02 06:49:40 -0400

    Permitir host Render en Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: true, // permite conexiones externas
+    port: 4173, // puerto de Vite, no crítico para Render
+    allowedHosts: ['js-movies.onrender.com'], // tu dominio en Render
+  }
+});
